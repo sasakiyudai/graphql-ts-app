6a8f23899b459aea51427981cee508c5f4e9126b
sasakiyudai <yudai14142@gmail.com>
2022-06-25 11:09:56 +0900

    voting

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -6,12 +6,21 @@ export const Link = objectType({
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+	t.nonNull.dateTime("createdAt");
     t.field("postedBy", {
       type: "User",
       resolve(parent, args, context) {
         return context.prisma.link
           .findUnique({ where: { id: parent.id } })
           .postedBy();
+      },
+    });
+    t.nonNull.list.nonNull.field("voters", {
+      type: "User",
+      resolve(parent, args, context) {
+        return context.prisma.link
+          .findUnique({ where: { id: parent.id } })
+          .voters();
       },
     });
   },
